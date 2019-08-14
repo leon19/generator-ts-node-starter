14033e5561f95cb804bd20632902c9285fc82e20
Lorens Leon <lorensleon@gmail.com>
2019-08-14 15:12:49 +0200

    chore: update dependencies
    BREAKING CHANGE: support for node >= 10

--- src/types.d.ts
+++
@@ -1,9 +0,0 @@
-declare module 'yeoman-assert' {
-  export function file(file: string): void;
-
-  export function noFile(file: string): void;
-
-  export function jsonFileContent(file: string, partialJson: object): void;
-
-  export function fileContent(file: string, contents: string): void;
-}
